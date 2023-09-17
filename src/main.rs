0ad67ea7ef077e218ae19cb8fb875f14f72bfd3e
5CYTH3 <ScytheD@outlook.fr>
2023-09-18 00:06:56 +0200

    Scaled the character without nearest color mixing

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
 mod player;
 
-use bevy::{prelude::{App, Startup}, DefaultPlugins};
+use bevy::{prelude::{App, Startup, Query, With, Commands, Camera2dBundle, Transform, ImagePlugin, PluginGroup}, window::{Window, PrimaryWindow}, utils::default, DefaultPlugins};
 
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins)
-    .add_systems(Startup, player::setup).run()
+    App::new()
+    .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
+    .add_systems(Startup, spawn_camera)
+    .add_systems(Startup, player::spawn_player)
+    .run()
 
+}
 
+fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<PrimaryWindow>>) {
+    let window = window_query.get_single().unwrap();
+    commands.spawn(
+        Camera2dBundle {
+            transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+            ..default()
+        }
+    );
 }
--- src/player.rs
+++ src/player.rs
@@ -1,12 +1,26 @@
-use bevy::{prelude::{Component, With}, ecs::system::Command, window::{Window, PrimaryWindow}};
+use bevy::ecs::system::Res;
+use bevy::prelude::{Transform, Vec2};
+use bevy::sprite::{SpriteBundle, Sprite};
+use bevy::{prelude::{Component, With, Commands, Query, AssetServer}, window::{Window, PrimaryWindow}};
 
 #[derive(Component)]
-pub struct Player;
+pub struct Player {}
 
-pub fn setup() {
-    println!("The player system still works")
-}
+pub fn spawn_player(mut commands: Commands, query: Query<&Window, With<PrimaryWindow>>, asset_server: Res<AssetServer>) {
+    let window = query.get_single().unwrap();
 
-pub fn spawn_player(mut command: Command, query: Query<&Window, With<PrimaryWindow>>) {
-
+    commands.spawn( 
+    (
+            SpriteBundle {
+                transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+                texture: asset_server.load("HeroIdleFrontSprite.png"),
+                sprite: Sprite {
+                    custom_size: Some(Vec2::new(128.0, 128.0)),
+                    ..Default::default()
+                },
+                ..Default::default()
+            },
+            Player {},
+        )
+    );
 }
